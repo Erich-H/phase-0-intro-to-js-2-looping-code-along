0631a74d4f176c639c79d47e10190944f03cafcb
Erich <eheiseler@gmail.com>
2022-02-23 19:51:57 -0500

    commit tree ad284267a5bb78d305deaa9f3aad369394b8aca8

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+for(let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+}
+
+function writeCards(names, event) {
+    const arr = []
+    for(let i = 0; i < names.length; i++) {
+         arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+         console.log(arr[i])
+    }
+    return arr
+}
+const list = ['fred','charles']
+writeCards(list, 'birthday' )
+
+function countDown(timer) {
+   while(timer > -1) {
+       console.log(timer--)
+   }
+}
